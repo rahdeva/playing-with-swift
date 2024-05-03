1374738c275b12d92fc5e92b510ef76b47ba7806
Ngurah Deva <wirandanaputra@gmail.com>
2024-05-03 10:19:27 +0700

    Initial Commit

---
+++ PlayingSwiftUI/PlayingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingSwiftUIApp.swift
+//  PlayingSwiftUI
+//
+//  Created by MacBook Air on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
